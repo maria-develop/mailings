88df133b8e23ec3c0d8b006e7e01ae49c534e3e8
Maria Ivanova <moi066@mail.ru>
2024-10-13 17:15:19 +0300

    убраны ключи, пароли в .env

--- config/settings.py
+++ config/settings.py
@@ -1,20 +1,34 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+# Загружаем переменные окружения из файла .env
+load_dotenv(override=True)
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+DEBUG = True if os.getenv('DEBUG') == 'True' else False
+
+ALLOWED_HOSTS = ["*"]
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+($hgj-(w059n4yt5voqo8jvhwbxm+d&8-nnc^itih)5hn@6z#'
+# SECRET_KEY = 'django-insecure-+($hgj-(w059n4yt5voqo8jvhwbxm+d&8-nnc^itih)5hn@6z#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -74,16 +88,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": 'mailings',
-        "USER": 'mailings_user',
-        "PASSWORD": 'Vawamawa1982',
-        "HOST": '127.0.0.1',
-        "PORT": '5432',
-        # "NAME": os.getenv('NAME'),
-        # "USER": os.getenv('USER'),
-        # "PASSWORD": os.getenv('PASSWORD'),
-        # "HOST": os.getenv('HOST'),
-        # "PORT": os.getenv('PORT'),
+        "NAME": os.getenv('NAME'),
+        "USER": os.getenv('USER'),
+        "PASSWORD": os.getenv('PASSWORD'),
+        "HOST": os.getenv('HOST'),
+        "PORT": os.getenv('PORT'),
     }
 }
 
